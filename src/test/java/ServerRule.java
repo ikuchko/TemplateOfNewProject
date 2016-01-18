8bc1e17e629965dbe6589119c2c46d67f217d728
ikuchko <ikuchko@gmail.com>
2016-01-17 21:03:11 -0800

    make more like a template.

--- src/test/java/ServerRule.java
+++ src/test/java/ServerRule.java
@@ -5,7 +5,7 @@ public class ServerRule extends ExternalResource {
 
   protected void before() {
       String[] args = {};
-      RPSGame.main(args);
+      App.main(args);
    }
 
   protected void after() {
